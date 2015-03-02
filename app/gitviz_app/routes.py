9c82d853ecac980d6a1fe563ad39f528086214e0
Roxana del Toro <rxdeltoro@gmail.com>
2015-03-02 12:28:03 -0800

    Clean up of doubled up files

--- app/gitviz_app/routes.py
+++ app/gitviz_app/routes.py
@@ -12,6 +12,7 @@ app.secret_key = os.urandom(24)
 
 app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
 app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
+app.config['GITHUB_TOKEN'] = os.environ['GITHUB_TOKEN']
 github = GitHub(app)
 
 login_manager = LoginManager()
@@ -25,6 +26,7 @@ def load_user(userid):
 @app.route('/', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+
   if form.validate_on_submit():
     user = session.query(User).filter_by(username=form.username.data).first()
     if user:
@@ -33,12 +35,11 @@ def login():
         flask_session['username'] = form.username.data
         login_user(user, remember=True)
         flash(u'Successfully logged in as %s' % form.user.username)
-        return redirect(url_for('visualize'))
+        return github.authorize()
+
   return render_template('login.html', form=form)
-  # return github.authorize()
 
 @app.route("/logout")
-@login_required
 def logout():
   user = current_user
   user.authenticated = False
@@ -58,9 +59,9 @@ def signup():
         newuser = User(form.username.data, form.password.data)
         session.add(newuser)
         session.commit()
-        flask_session['username'] = newuser.username
+        login_user(user, remember=True)
         flash('User successfully signed up.')
-        return redirect(url_for('visualize'))
+        return github.authorize()
       except:
         flash('There was an issue creating the user. Try again')
         return render_template('signup.html', form=form)
@@ -68,37 +69,37 @@ def signup():
   elif request.method == 'GET':
     return render_template('signup.html', form=form)
 
-@login_required
-@app.route('/visualize')
-def visualize():
-  if 'username' not in flask_session:
-    return redirect(url_for('login'))
-  # return render_template('visualize.html', client_id=app.config['GITHUB_CLIENT_ID'], client_secret=app.config['GITHUB_CLIENT_SECRET'])
-  # return github.authorize()
-
 @github.access_token_getter
 def token_getter():
-  user = current_user
-  if user is not None:
-    return user.github_access_token
+  if current_user is not None:
+    return current_user.github_access_token
 
 @app.route('/callback') #http://goo.gl/s8pKEs
 @github.authorized_handler
-def authorized(oauth_token):
-  if oauth_token is None:
-    flash("Authorization failed.")
-    return redirect(url_for('visualize'))
-  else:
-    flash("Successfully authorized")
+def authorized(access_token):
+    if not current_user.is_authenticated:
+      print '1 1 1'
+      return redirect(url_for('login'))
 
-  user = User.query.filter_by(github_access_token=oauth_token).first()
-  if user is None:
-      user = User(oauth_token)
-      session.add(user)
+    # next_url = request.args.get('next') or render_template('visualize.html')
+    print '2 2 2'
 
-  user.github_access_token = oauth_token
-  session.commit()
-  return redirect(next_url)
+    if access_token is None:
+      print '3 3 3'
+      # return redirect(next_url)
+      return render_template('visualize.html', client_id=os.environ['GITHUB_CLIENT_ID'], client_secret=os.environ['GITHUB_CLIENT_SECRET'])
+
+    current_user.github_access_token = oauth_token
+    print '4 4 4'
+    session.commit()
+    print '5 5 5'
+
+    return render_template('visualize.html')
+
+
+@app.route('/user')
+def user():
+    return str(github.get('current_user'))
 
 if __name__ == '__main__':
   app.run(debug=True)  
